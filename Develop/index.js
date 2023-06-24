ea1bb82626e117f38961229a3f0472bb782c2d1e
minkiko@gmail.com <minkiko@gmail.com>
2023-06-24 13:03:00 -0500

    added addtional questions to the array

--- Develop/index.js
+++ Develop/index.js
@@ -14,6 +14,46 @@ const questions = [
         name: 'github',
         message: "What's your GitHub account username?"
     },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: "What's your email?"
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: "What's the name of your project?"
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please provide a brief description of your project?"
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: "Choose the appropriate license for your application",
+        choices: ["Apache 2.0", "MIT License", "GNU GPLv3"]
+    },
+
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: "What dependencies need to be install prior to running?",
+        default: 'npm i'
+    },
+
+    {
+        type: 'input',
+        name: 'test',
+        message: "What command do you enter to run the test?",
+        default: 'npm test'
+    }
+    
 ];
 
 // TODO: Create a function to write README file
